61b46365c90a241a61fda7b7457c0cad6bf5721c
viju-dev <vijaydorugade20@gmail.com>
2023-01-21 16:13:38 +0530

    updated files

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -54,7 +54,7 @@ public class Car extends Vehicle {
     }
 
     public void changeGear(int newGear){
-        this.gears = newGear;
+        this.currentGear = newGear;
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -5,6 +5,7 @@ public class F1 extends Car {
     public F1(String name, boolean isManual) {
         super(name,isManual);
         //Use arbitrary values for parameters which are not mentioned
+
     }
 
     public void accelerate(int rate){
@@ -18,16 +19,38 @@ public class F1 extends Car {
          * speed 201-250: gear 5
          * speed more than 250: gear 6
          */
-        newSpeed += rate;
+        newSpeed = getCurrentSpeed()+rate;
+//        System.out.println(newSpeed);
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
-            stop();
             changeGear(1);
+            stop();
+
         }
         //for all other cases, change the gear accordingly
 
         if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
+            if (newSpeed >=1 && newSpeed<=50){
+                changeGear(1);
+            }
+            else if (newSpeed >=51 && newSpeed<=100){
+                changeGear(2);
+            }
+            else if (newSpeed >=101 && newSpeed<=150){
+                changeGear(3);
+            }
+            else if (newSpeed >=151 && newSpeed<=200){
+                changeGear(4);
+            }
+            else if (newSpeed >=200 && newSpeed<=250){
+                changeGear(5);
+            }
+            else if (newSpeed > 250){
+                changeGear(6);
+            }
         }
+
+
     }
 }
